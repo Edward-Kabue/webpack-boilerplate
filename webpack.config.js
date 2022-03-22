86d8efe8702aa73eb1f865974609f57a4bda3c91
Edward Kabue <ekabue0@gmail.com>
2022-03-22 06:35:00 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist/index.html'),
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
